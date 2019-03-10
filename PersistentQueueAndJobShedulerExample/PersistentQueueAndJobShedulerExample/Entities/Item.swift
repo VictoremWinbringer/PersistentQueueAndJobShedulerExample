0697924d2778d4f9b90275b7665c69351b806822
Victor Winbringer <victorwinbringer@MacBook-Pro-Victor.local>
2019-03-10 16:57:25 +0300

    added Item entity class

---
+++ PersistentQueueAndJobShedulerExample/PersistentQueueAndJobShedulerExample/Entities/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  PersistentQueueAndJobShedulerExample
+//
+//  Created by Victor Winbringer on 10/03/2019.
+//  Copyright © 2019 Victor Winbringer. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var id:Int
+    var name:String
+    
+    init() {
+        self.id = 0
+        self.name = "unown"
+    }
+}
