dfd023dec43ed021657b08787ccaff63ee88d5a7
Victor Winbringer <victorwinbringer@MacBook-Pro-Victor.local>
2019-03-10 21:51:06 +0300

    added ItemsTableViewController

---
+++ PersistentQueueAndJobShedulerExample/PersistentQueueAndJobShedulerExample/ItemsTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ItemsTableViewController.swift
+//  PersistentQueueAndJobShedulerExample
+//
+//  Created by Victor Winbringer on 10/03/2019.
+//  Copyright © 2019 Victor Winbringer. All rights reserved.
+//
+
+import UIKit
+
+class ItemsTableViewController: UITableViewController {
+    var items:[String] = Array.init(repeating: "Test!", count: 10)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+        //    self.navigationItem.leftBarButtonItem = self.in
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return items.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction.init(style: .normal, title: "Delete", handler: { a, i in
+            
+        })
+        delete.backgroundColor = UIColor.blue
+        let update = UITableViewRowAction.init(style: .normal, title: "Update", handler: { a, i in
+            
+        })
+        update.backgroundColor = UIColor.red
+        return [update,delete]
+    }
+}
---
+++ PersistentQueueAndJobShedulerExample/PersistentQueueAndJobShedulerExample/Ports/ViewControllerPort.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerPort.swift
+//  PersistentQueueAndJobShedulerExample
+//
+//  Created by Victor Winbringer on 10/03/2019.
+//  Copyright © 2019 Victor Winbringer. All rights reserved.
+//
+
+import Foundation
+
+class ViewControllerPort {
+    
+}
